6e872763a47553c4c769fac1cf4f06efe47c817d
Derek Wood <derek.wood@datastrong.com>
2022-05-19 16:30:18 -0400

    0.3.3 Fix S3

--- config_wrangler/config_templates/s3_bucket.py
+++ config_wrangler/config_templates/s3_bucket.py
@@ -1,6 +1,5 @@
-from copy import copy
-from typing import *
 from pathlib import PurePosixPath, Path
+from typing import *
 
 from boto3.s3.transfer import TransferConfig
 from pydantic import PrivateAttr
@@ -44,10 +43,10 @@ class S3_Bucket(AWS_Session):
         extra_args: Optional[dict] = None,
         transfer_config: Optional[TransferConfig] = None,
     ):
-        self.resource.upload_file(
-            Filename=local_filename,
+        self.client.upload_file(
+            Filename=str(local_filename),
             Bucket=self.bucket_name,
-            Key=key,
+            Key=str(key),
             ExtraArgs=extra_args,
             Config=transfer_config,
         )
@@ -61,10 +60,13 @@ class S3_Bucket(AWS_Session):
     ):
         self.resource.Bucket(self.bucket_name).download_file(
             Key=str(key),
-            Filename=local_filename,
+            Filename=str(local_filename),
             ExtraArgs=extra_args,
             Config=transfer_config,
         )
+
+    def get_object(self, key: Union[str, PurePosixPath]):
+        return self.resource.Object(self.bucket_name, str(key))
 
     def delete_by_key(self, key: Union[str, PurePosixPath]):
         self.client.delete_object(Bucket=self.bucket_name, Key=str(key))
@@ -72,7 +74,18 @@ class S3_Bucket(AWS_Session):
     def list_object_keys(self, key: Union[str, PurePosixPath]) -> List[str]:
         paginator = self.client.get_paginator('list_objects_v2')
         response = paginator.paginate(Bucket=self.bucket_name, Prefix=key)
-        return [object['Key'] for object in response['Contents']]
+        return [obj['Key'] for obj in response['Contents']]
+
+    def get_copy(self, copied_by: str = 'get_copy') -> 'S3_Bucket':
+        new_instance = self.copy(deep=False)
+        new_instance._root_config = self._root_config
+        new_instance._parents = self._parents + [copied_by]
+        return new_instance
+
+    def nav_to_bucket(self, bucket_name) -> 'S3_Bucket':
+        new_instance = self.get_copy()
+        new_instance.bucket_name = bucket_name
+        return new_instance
 
 
 class S3_Bucket_Folder(S3_Bucket):
@@ -111,19 +124,23 @@ class S3_Bucket_Folder(S3_Bucket):
             transfer_config=transfer_config,
         )
 
-    def get_copy(self, copied_by: str = 'get_copy') -> 'S3_Bucket_Folder':
-        new_folder = self.copy(deep=True)
-        new_folder._root_config = self._root_config
-        new_folder._parents = self._parents + [copied_by]
-        return new_folder
-
     def joinpath(self, *other) -> 'S3_Bucket_Folder':
         new_folder = self.get_copy(copied_by=f".join({other})")
         new_key = PurePosixPath(self.folder, *other)
         new_folder.folder = str(new_key)
         return new_folder
 
-    def list_object_keys(self, key: Union[str, PurePosixPath]) -> List[str]:
+    def __truediv__(self, other) -> 'S3_Bucket_Folder':
+        return self.joinpath(other)
+
+    def key_exists(self, key: Union[str, PurePosixPath]):
+        try:
+            self.client.head_object(Bucket=self.bucket_name, Key=str(key))
+            return True
+        except Exception:
+            return False
+
+    def list_object_keys(self, key: Union[str, PurePosixPath] = None) -> List[str]:
         if key is None:
             key = self.folder
         return super().list_object_keys(key=key)
